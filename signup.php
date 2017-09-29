17f1703f66408ca294f9dfb7eb65518be51f89fb
Anthony Roussel <anthony.roussel17@gmail.com>
2017-09-29 14:41:04 +0300

    add put and delete methods for users

--- signup.php
+++ signup.php
@@ -1,24 +1,22 @@
 <?php
-
-// Include confi.php
 include_once('config.php');
 
-if($_SERVER['REQUEST_METHOD'] == "POST"){
-$name = (isset($_POST['name']))?$_POST['name']:'' ;
-$email = (isset($_POST['email']))?$_POST['email']:'';
-$password = (isset($_POST['password']))?$_POST['password']:'';
-$status = (isset($_POST['status']))?$_POST['status']:'';
-$sql = "INSERT INTO users (`name`, `email`, `password`, `status`) VALUES     ('$name', '$email', '$password', '$status')";
-$qur = mysqli_query($conn, $sql);
-if($qur){
-$json = array("status" => 1, "msg" => "Done User added!");
-}else{
-$json = array("status" => 0, "msg" => "Error adding user!");
-}
-}else{
-$json = array("status" => 0, "msg" => "Request method not accepted");
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+  $name = (isset($_POST['name']))?$_POST['name']:'' ;
+  $email = (isset($_POST['email']))?$_POST['email']:'';
+  $password = (isset($_POST['password']))?$_POST['password']:'';
+  $status = (isset($_POST['status']))?$_POST['status']:'';
+  $sql = "INSERT INTO users (name, email, password, status) VALUES ('$name', '$email', '$password', '$status')";
+  $qur = mysqli_query($conn, $sql);
+  if ($qur) {
+    $json = array("status" => 1, "msg" => "Done User added!");
+  } else {
+    $json = array("status" => 0, "msg" => "Error adding user!");
+  }
+} else {
+  $json = array("status" => 0, "msg" => "Request method not accepted");
 }
 @mysqli_close($conn);
 header('Content-type: application/json');
 echo json_encode($json);
-	
+    
